a27ab355d274a18ef2d0dc8d5d0c481f3dadca29
YannRodriguez <52059549+YannRodriguez@users.noreply.github.com>
2021-12-31 14:29:05 +0100

    update

--- saver.py
+++ saver.py
@@ -1,25 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def save(name,array):
     with open(f'item/{name}.npy', 'wb') as f:
         np.save(f, array)
-
-
-array = np.array([
-    [0,1,1,0,0,0,0,0,0,0],
-    [0,0,1,0,0,0,0,0,0,0],
-    [0,0,1,0,1,1,0,0,0,0],
-    [0,1,1,0,1,0,1,0,0,0],
-    [0,0,0,0,0,0,0,1,0,0],
-    [0,0,0,0,0,1,1,1,0,0],
-    [1,1,0,0,0,0,0,0,1,1],
-    [1,0,0,1,0,1,1,0,1,0],
-    [0,0,1,1,0,1,0,0,1,0],
-    [0,0,0,0,0,0,1,1,0,0],
-],dtype=bool)
-
-plt.imshow(array)
-plt.show()
-
-save('',array)
